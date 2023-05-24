34b83ab6ed177ceefd97ce100cfdd1e426aac3da
Joshua Onwuemene <emjay6299@gmail.com>
2023-05-24 10:02:31 +0200

    Update Comment.ts: corrected indentation

--- model/Comment.ts
+++ model/Comment.ts
@@ -4,9 +4,9 @@ type Comment = InferSchemaType<typeof Schema>
 
 const commentSchema = new Schema<Comment>({
 	comment: {
-        	type: String,
-			required: true
-    	},
+		type: String,
+		required: true
+    },
    	author: {
 		type: Schema.Types.ObjectId,
 		ref: "Employee"
